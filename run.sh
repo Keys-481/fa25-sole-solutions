5acacaa7d5a168e1ae34899ac9fe0badd4b426b6
Ryan Macfarlane <101445807+RyMacfly@users.noreply.github.com>
2025-09-25 11:53:55 -0600

    Setup project structure (#63)
    * Setup project structure
    
    * changed to pytest

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the main application
+python -m src.main
---
+++ tests/test_placeholder.py
@@ -0,0 +1,9 @@
+from src.ui.app import run_ui
+
+def test_placeholder():
+    """
+    Dummy test to show structure and how to run tests.
+    This test will be deleted later.
+    """
+    # Just a placeholder assertion
+    assert True
